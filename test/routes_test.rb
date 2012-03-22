6c056f3d01dc56d54e7203dc70bb475ae0b65e73
Randy Morgan <randym@reallyenglish.com>
2012-03-23 00:11:02 +0900

    patch specs

--- test/routes_test.rb
+++ test/routes_test.rb
@@ -5,13 +5,13 @@ class RoutesTest < ActionController::TestCase
   puts LocaleappI18nJs::Engine.routes.routes.inspect
   test 'localeapp_i18n_js update is set' do
     assert_generates "localeapp_i18n_js", {
-      :controller => "LocaleappI18nJs",
+      :controller => "LocaleappI18nJs/LocaleappI18nJs",
       :action => "update"}
   end
 
   test 'localeapp_i18n_js show is set' do
-    assert_generates "/localeapp_i18n_js", {
-      :controller => "LocaleappI18nJs",
+    assert_generates "localeapp_i18n_js", {
+      :controller => "LocaleappI18nJs/LocaleappI18nJs",
       :action => "show"}
   end
 
