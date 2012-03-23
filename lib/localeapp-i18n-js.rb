c833ec9db2ef2cec0711da404b00c761e4c3e94f
Randy Morgan <randym@reallyenglish.com>
2012-03-24 01:26:45 +0900

    helper class method to ensure that localeapp is configured for sending missing translations.

--- lib/localeapp-i18n-js.rb
+++ lib/localeapp-i18n-js.rb
@@ -1,1 +1,7 @@
 require 'localeapp-i18n-js/engine'
+module LocaleappI18nJs
+  extend self
+  def localeapp_initialized?
+    defined?(Localeapp) && Localeapp.configuration.respond_to?(:sending_disabled?)
+  end
+end
