e8734194303a49ff2595650dc34eba7230a01c54
Randy Morgan <randym@reallyenglish.com>
2012-03-24 00:54:09 +0900

    remove debug code from routes tests

--- test/routes_test.rb
+++ test/routes_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class RoutesTest < ActionController::TestCase
-  include Rails.application.routes.url_helpers
-  puts LocaleappI18nJs::Engine.routes.routes.inspect
   test 'localeapp_i18n_js update is set' do
     assert_generates "localeapp_i18n_js", {
       :controller => "LocaleappI18nJs/LocaleappI18nJs",
