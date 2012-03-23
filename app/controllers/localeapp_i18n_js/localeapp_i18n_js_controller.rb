4928ff4fdb63a5936e34489c1d5c0fdc7047b87e
Randy Morgan <randym@reallyenglish.com>
2012-03-24 01:29:52 +0900

    only inject controller if localeapp is initialized

--- app/controllers/localeapp_i18n_js/localeapp_i18n_js_controller.rb
+++ app/controllers/localeapp_i18n_js/localeapp_i18n_js_controller.rb
@@ -1,23 +1,25 @@
-module LocaleappI18nJs
-  class LocaleappI18nJsController < ActionController::Base
+if LocaleappI18nJs.localeapp_initialized?
+  module LocaleappI18nJs
+    class LocaleappI18nJsController < ActionController::Base
 
-    def show
-      render "localeapp_i18n_js/show", :layout => "application"
+      def show
+        render "localeapp_i18n_js/show", :layout => "application"
+      end
+
+      def update
+        Localeapp.log "[LocaleappI18nJs] update: locale=#{params[:locale]}, key=#{params[:key]}"
+        add_missing_translation params[:locale], params[:key] unless Localeapp.configuration.sending_disabled?
+        render :json => true
+      end
+
+      protected
+
+      def add_missing_translation(locale, key)
+        Localeapp.missing_translations.add locale, key
+        Localeapp.sender.post_missing_translations
+      end
+
     end
-
-    def update
-      Localeapp.log "[LocaleappI18nJs] update: locale=#{params[:locale]}, key=#{params[:key]}"
-      add_missing_translation params[:locale], params[:key] unless Localeapp.configuration.sending_disabled?
-      render :json => true
-    end
-
-    protected
-
-    def add_missing_translation(locale, key)
-      Localeapp.missing_translations.add locale, key
-      Localeapp.sender.post_missing_translations
-    end
-
   end
 end
 
